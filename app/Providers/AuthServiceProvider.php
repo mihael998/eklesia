12491725e19b007503f7424f97a423317a00a187
mihael998 <isamihael@gmail.com>
2018-02-13 20:54:29 +0100

    Modifica scadenza token per l'autenticazione

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -49,7 +49,7 @@ class AuthServiceProvider extends ServiceProvider
             $router->forTransientTokens();
         });
         
-        Passport::tokensExpireIn(Carbon::now()->addMinutes(10));
+        Passport::tokensExpireIn(Carbon::now()->addDays(1));
         
         Passport::refreshTokensExpireIn(Carbon::now()->addDays(10));
         
